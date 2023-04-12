31a4480ddc3ec01b219796150282b47e5b0c1180
Prince Narteh <prinarteh@gmail.com>
2023-04-12 16:12:23 +0000

    add middlewares

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,24 @@
-import * as express from "express";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
 import * as dotenv from "dotenv";
+import * as express from "express";
+import fileUpload from "express-fileupload";
+import ErrorHandler from "./utils/errorHandler";
 
 const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(fileUpload({ useTempFiles: true }));
 
 if (process.env.NODE_ENV !== "production") {
   dotenv.config({
     path: "./src/config/.env",
   });
 }
+
+// Error handling
+app.use(ErrorHandler);
 
 export { app };
---
+++ src/middleware/auth.ts
@@ -0,0 +1,3 @@
+import ErrorHandler from "../utils/errorHandler";
+import catchAsyncErrors from "./catchAsyncErrors";
+import jwt from "jsonwebtoken";
