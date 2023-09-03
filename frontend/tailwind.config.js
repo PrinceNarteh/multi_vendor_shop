f8f94944feff918cd332e9d95176fa90b9ca11bc
Prince Narteh <prinarteh@gmail.com>
2023-09-03 17:41:22 +0000

    added tailwind and fonts

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  mode: "jit",
   theme: {
-    extend: {},
+    fontFamily: {
+      "poppins-bold": ["Poppins-Bold", "sans-serif"],
+      "poppins-regular": ["Poppins-Regular", "sans-serif"],
+      "roboto-bold": ["Roboto-Bold", "sans-serif"],
+      "roboto-regular": ["Roboto-Regular", "sans-serif"],
+    },
+    extend: {
+      screens: {
+        "400px": "400px",
+        "800px": "800px",
+        "1000px": "1050px",
+        "1100px": "1110px",
+        "1300px": "1300px",
+      },
+    },
   },
   plugins: [],
 };
